c25a7d0acf09cbe2ed207bd61b929a40b9c4c99f
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-04-23 10:39:16 -0500

    done

--- lib/trainers/trainer.py
+++ lib/trainers/trainer.py
@@ -5,9 +5,9 @@ import torch
 class Trainer:
     def __init__(self, experiment):
         self.experiment = experiment
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        print(self.device)
-        self.logger = Logger
+        self.device = torch.device('cuda' if torch.cuda.
+                                   is_available() else 'cpu')
+        self.logger = Logger(self.experiment.stats_folder)
 
     def train(self):
         raise NotImplementedError
