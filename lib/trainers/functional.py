d84288aaf8d2d36032b3a57662b4bccdb652364a
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-04-23 07:09:11 -0500

    claen submodule

---
+++ lib/trainers/functional.py
@@ -0,0 +1,36 @@
+import torch
+import torch.functional as F
+
+
+def cross_entropy2d(output, target, weight=None):
+    n, c, h, w = output.size()
+    nt, ht, wt = target.size()
+
+    # Handle inconsistent size between output and target
+    if h != ht and w != wt:  # upsample labels
+        output = F.interpolate(
+            output, size=(ht, wt), mode="bilinear", align_corners=True)
+
+    output = output.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    target = target.view(-1)
+    loss = F.cross_entropy(output, target, weight=weight)
+
+    return loss
+
+
+def get_iou(outputs, labels):
+    SMOOTH = 1e-6
+    # You can comment out this line if you are passing tensors of equal shape
+    # But if you are passing output from UNet or something it will most
+    # probably be with the BATCH x 1 x H x W shape
+    outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
+    intersection = (outputs & labels).float().sum(
+        (1, 2))  # Will be zero if Truth=0 or Prediction=0
+    union = (outputs | labels).float().sum((1,
+                                            2))  # Will be zzero if both are 0
+    iou = (intersection + SMOOTH) / (union + SMOOTH
+                                     )  # We smooth our devision to avoid 0/0
+    thresholded = torch.clamp(
+        20 * (iou - 0.5), 0,
+        10).ceil() / 10  # This is equal to comparing with thresolds
+    return thresholded.mean()
--- lib/trainers/trainer.py
+++ lib/trainers/trainer.py
@@ -1,6 +1,52 @@
+from tqdm import tqdm
+from lib.simple_ml_logger.logger import Logger
+import torch
+from pathlib import Path
+
+
 class Trainer:
     def __init__(self, experiment):
-        raise NotImplementedError
+        self.device = torch.device('cuda' if torch.cuda.
+                                   is_available() else 'cpu')
+        self.logger = Logger
+        self.experiment = experiment
 
     def train(self):
+        self._setup_model()
+        self._setup_optimizer()
+        self._load_checkpoint()
+        for epoch in tqdm(
+                range(self.start_epochs, self.experiment.config["epochs"])):
+
+            total_loss = 0
+            self.model.train()
+            for X, Y in tqdm(self.train_loader):
+                X, Y = X.to(self.device), Y.to(self.device)
+                total_loss += self._step(X, Y)
+            avg_loss = total_loss / len(self.train_loader)
+            self.logger.log("epoch", "avg_loss", epoch, avg_loss)
+
+            val_metric = self.validate()
+            self.logger.log("epoch", "val_metric", epoch, val_metric)
+
+    def _step(self, X, Y):
+        raise NotImplementedError
+
+    def validate(self):
+        """
+            returns:
+                val_metric: your favorite evaluation metric
+        """
+        raise NotImplementedError
+
+    def _load_checkpoint(self):
+        self.start_epochs = 0
+        if self.experiment.config["checkpoint path"]:
+            self.start_epochs = int(Path(self.checkpoint_path).stem)
+            self.model.load_state_dict(torch.load(self.checkpoint_path))
+
+    def _setup_model(self):
+        raise NotImplementedError
+
+    def _setup_optimizer(self):
         raise NotImplementedError
